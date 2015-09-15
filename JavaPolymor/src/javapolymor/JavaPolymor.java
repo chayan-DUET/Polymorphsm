5eaf04e85f3a1e7f1d6f41b6739389f0869d99b2
chayan-DUET <chayan_cse10@yahoo.com>
2015-09-15 09:57:04 +0600

    polymorphic
    just create polymorphic runtime error

---
+++ JavaPolymor/src/javapolymor/JavaPolymor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapolymor;
+
+/**
+ *
+ * @author Chayan_pc
+ */
+public class JavaPolymor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Mypolymor m=new Mypolymor();
+        m.fun();
+       m.fun(10);        //ompile time polymorphic
+    }
+}
---
+++ JavaPolymor/src/javapolymor/Mypolymor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapolymor;
+
+/**
+ *
+ * @author Chayan_pc
+ */
+public class Mypolymor {
+    void fun()
+    {
+        System.out.println("this is 1st method fun() with out value");
+    }
+   void fun(int x)
+   {
+      // x=5;
+       System.out.println("this is 2nd method fun() with value"+x);
+   
+   }
+    
+}
